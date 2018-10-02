547fc959c7043fbe7235c76eaff0ec7c91eca545
Tonni Lutze <tonni@witzell.dk>
2018-10-02 14:48:24 +0200

    Additional code updated.

--- ATM/Separation.cs
+++ ATM/Separation.cs
@@ -4,16 +4,22 @@ namespace ATM
 {
     public class Separation 
     {
-        public Separation()
+        public Separation(string planeTag1, string planeTag2, DateTime separationEventTimestamp)
         {
+            PlaneTag1 = planeTag1;
+            PlaneTag2 = planeTag2;
+            
+
         }
-        public DateTime SeparationEventTimestamp { get; private set; }
+        public DateTime SeparationEventTimestampStart{get; private set; }
+        public DateTime SeparationEventTimestampStop { get; private set; }
         public string PlaneTag1 { get; private set; }
+        
         public string PlaneTag2 { get; private set; }
 
 
 
-
+        
 
     }
 }
