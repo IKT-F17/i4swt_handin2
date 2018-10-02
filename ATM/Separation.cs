31a83c9662b32b1b126cb570ef642a8bdb534e92
Tonni Lutze <tonni@witzell.dk>
2018-10-02 15:26:16 +0200

    Commit before  going home

--- ATM/Separation.cs
+++ ATM/Separation.cs
@@ -4,14 +4,13 @@ namespace ATM
 {
     public class Separation 
     {
-        public Separation(string planeTag1, string planeTag2, DateTime separationEventTimestamp)
+        public Separation(ITrack track1, ITrack track3)
         {
-            PlaneTag1 = planeTag1;
-            PlaneTag2 = planeTag2;
             
 
         }
-        public DateTime SeparationEventTimestampStart{get; private set; }
+        public DateTime SeparationEventTimestampStart
+        { get; private set; }
         public DateTime SeparationEventTimestampStop { get; private set; }
         public string PlaneTag1 { get; private set; }
         
