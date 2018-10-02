6513373c41b1b49bfcc6351dab9bff5f65010d9b
Tonni Lutze <tonni@witzell.dk>
2018-10-02 14:10:28 +0200

    Fixed misc errors

--- ATM/Separation.cs
+++ ATM/Separation.cs
@@ -12,8 +12,11 @@ namespace ATM
         public Separation()
         {
         }
+        public DateTime SeparationEventTimestamp { get; private set; }
+        public string PlaneTag1 { get; private set; }
+        public string PlaneTag2 { get; private set; }
 
-        public string firstPlaneTag { get; }
+
 
 
 
