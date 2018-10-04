fed24b0ae206258fa1df2c2da391241f5a234f52
Tonni Lutze <tonni@witzell.dk>
2018-10-04 12:06:23 +0200

    Added Nuget Package

--- ATM.Unit.Test/ATM_Unit_Test.cs
+++ ATM.Unit.Test/ATM_Unit_Test.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+
 
 namespace ATM.Unit.Test
 {
+
+
+    [TestFixture]
     public class ATM_Unit_Test
     {
+        private string RAWTESTDATA = "PIE284;29388;49932;2000;20151006213456789";
 
+        [Test]
+        public void SpawnTrackReturnsTrack()
+        {
+            //var uut = new TrackFactory();
+            //Assert.That(uut.SpawnTrack(RAWTESTDATA), SpawnTrackReturnsTrack() );
+        }
     }
 }
