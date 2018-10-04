90c7e19b15b5e56fcca613199fda65a3eaa0fd18
Thunbo <mthunbov2@gmail.com>
2018-10-04 13:35:28 +0200

    Added FakeTrackFactory

---
+++ ATM.Unit.Test/Fakes/FakeTrackFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Unit.Test.Fakes
+{
+    class FakeTrackFactory : ITrackFactory
+    {
+        public ITrack SpawnTrack(string rawTrackData)
+        {
+            string[] rawDataSplit = rawTrackData.Split(';');
+            string tag = rawDataSplit[0];   
+            int xcoord = Convert.ToInt32(rawDataSplit[1]);
+            int ycoord = Convert.ToInt32(rawDataSplit[2]);
+            int altitude = Convert.ToInt32(rawDataSplit[3]);
+            DateTime timeStamp =
+                DateTime.ParseExact(rawDataSplit[4], "yyyyMMddHHmmssfff", CultureInfo.InvariantCulture);
+
+            return new Track(tag, xcoord, ycoord, altitude, timeStamp);
+        }
+    }
+}
